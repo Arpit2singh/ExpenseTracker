4ed0d2886850291c2961d8a12036a339f5005b49
Arpit Singh <arpitno57@gmail.com>
2025-07-21 22:55:04 +0530

    auth middleware added

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import { User } from "../models/user.model";
+import ApiError from "../utils/ApiError";
+import asyncHandler from "../utils/asyncHandler";
+import  jwt  from "jsonwebtoken"; 
+
+const verifyJWT = asyncHandler(async(req , res ,next)=>{
+try {
+
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer " , "") 
+    if(!token){
+        throw new ApiError(404 , "Token not found ") ; 
+    }
+    const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET ) ; 
+    const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(404 , "invalid accessToken")
+    }
+    req.user = user ; 
+    next() ;
+    
+} catch (error) {
+    throw new ApiError(404 , `${error.message}`||`jwt middleware error`)
+}
+})
